2ee6df738cc73b6c17c403d92f0a0481039237d0
jmb11x <99753424+jmb11x@users.noreply.github.com>
2024-04-10 20:15:26 -0400

    initial logic going well; stopped because answer CHECKING logic is not working.

--- game.js
+++ game.js
@@ -1,92 +1,117 @@
-var buttonColors = [
-    "red",
-    "blue", 
-    "green", 
-    "yellow"
-]
-
-var gamePattern = [];
-var userClickedPattern = [];
-var gameStarted = false;
-var gameLevel = -1;
-var currentClick = 0;
-
-//simon gives the next item in sequence
-function nextSequence(){
-    //change what level it is
-    gameLevel++;
-    $("h1").html("Level " + gameLevel);
-    
-    //add new item to game pattern
-    var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
-
-    setTimeout(() => {
-        animateButtonForSimon(randomChosenColor);
-        playSoundForButtonWithColor(randomChosenColor); 
-    }, 1000);
-
-}
-
-//user enters entire sequence including new simon item
-$(".btn").click(function(event){
-    var userChosenColor = event.currentTarget.id;
-    userClickedPattern.push(userChosenColor);
-    animateButtonForUser(userChosenColor);
-    playSoundForButtonWithColor(userChosenColor);
+// background image selector
+let backgroundSelector = document.getElementById("background-img-selector")
+backgroundSelector.addEventListener('change', () => {
+  document.getElementById("body-elem").style.backgroundImage = `url(${backgroundSelector.value})`;
 })
 
-//ancillary functions
-function animateButtonForSimon(c){
-    $('#' + c).fadeOut();
-    $('#' + c).fadeIn();
-}
-function animateButtonForUser(c){
-    $('#' + c).addClass("pressed");
+// start button
+let startGameButton = document.getElementById("start-game-button")
+startGameButton.addEventListener('click', () => {
+    start_game();
+})
+
+
+
+// GAMING LOGIC
+// initialization
+var gamePattern;
+var userPattern;
+var gameLevel;
+var currentClick;
+var sound;
+var gameActive = false;
+
+var gamePieces = [
+    "gryffindor",
+    "slytherin",
+    "hufflepuff",
+    "ravenclaw"
+]
+var gamePieceActivationTimeout = 1000
+
+// game piece animations
+function activate_game_piece(game_piece_element){
+    // colorize game piece
+    $(game_piece_element).addClass("ignite");
     setTimeout(() => {
-        $('#' + c).removeClass("pressed");
-    }, 100);
-    currentClick++;
-    checkAnswer(c);
-}
-function playSoundForButtonWithColor(c){
-    var sound = new Audio("sounds/" + c + ".mp3");
+        $(game_piece_element).removeClass("ignite");
+    }, gamePieceActivationTimeout);
+
+    // play sound
+    var sound = new Audio('./sounds/confringo.mp3');
     sound.play();
 }
 
-//start game if this is first time key pressed
-$(document).keydown(function(key){
-    if (gameStarted == false){
-        gameStarted = true;
-        nextSequence();
-    }
-})
+// setup new game
+function setup_new_game(params) {
+    gamePattern = [];
+    userPattern = [];
+    gameStarted = false;
+    gameLevel = -1;
+    currentClick = -1;
+    gameActive = true;
+}
+function start_game(params) {
+    // reset memory
+    setup_new_game();
 
-//check answer
-function checkAnswer(color){
-    if (color != gamePattern[currentClick-1]){
-        gamePattern = [];
-        userClickedPattern = [];
-        gameStarted = false;
-        gameLevel = -1;
-        currentClick = 0;
-        playSoundForButtonWithColor("wrong");
-        $("h1").html("Oh no! That's wrong! Press any key to start again.")
-    }else{
-        if (currentClick == gamePattern.length){
-            currentClick = 0;
-            nextSequence();
-        }    
-    }
-    console.log("Pattern: " + gamePattern )
-    console.log("User: " + userClickedPattern )
-
+    // start pattern creation
+    ballow_says_next_game_piece();
 }
 
+// handle pattern (game)
+function ballow_says_next_game_piece(){
+    // increment level
+    gameLevel++;
 
-// let beat = new Audio('./sounds/main_theme.mp3');
+    // $("h1").html("Level " + gameLevel);
 
-// document.getElementById('play-game-button').addEventListener('click', () => {
-//     beat.play();
-// })
+    // append new element to pattern
+    var r = Math.floor(Math.random()*4);
+    var gamePieceId = gamePieces[r];
+    gamePattern.push(gamePieceId);
+
+    // show user selected game piece
+    setTimeout(() => {
+        activate_game_piece(document.getElementById(gamePieceId))
+    }, gamePieceActivationTimeout);
+    console.log(`Ballow says next piece is: ${gamePieceId}`)
+}
+
+function check_user_pattern_this_click(seleted_game_piece) {
+    // todo only permit execution of this if lock is not locked
+
+    // increment click count
+    currentClick++;
+
+    // animate
+    activate_game_piece(seleted_game_piece)
+
+    // check answer
+    check_selected_game_piece(seleted_game_piece.id)
+
+    // add answer to list
+}
+
+function check_selected_game_piece(game_piece_id){
+    console.log(`Checking Selection: ${game_piece_id}`)
+    if (gameActive) {
+        if (game_piece_id != gamePattern[currentClick]){
+            console.log("LOSE!")
+            gameActive = false;
+            sound = new Audio('./sounds/green.mp3');
+            // $("h1").html("Oh no! That's wrong! Press any key to start again.")
+        }else{
+            console.log("CORRECT PIECE.")
+            // they are equal so far, so add it to list
+            userPattern.push(game_piece_id)
+            if (currentClick+1 == gamePattern.length){
+                console.log("CORRECT SEQUENCE.")
+                currentClick = 0;
+                ballow_says_next_game_piece();
+            } else {
+                console.log("Keep clicking")
+            }
+        }
+    }
+}
--- scripts/effects.js
+++ scripts/effects.js
@@ -4,10 +4,6 @@ let welcomePage = document.getElementById('welcome-page')
 // main song playing in background
 let beat = new Audio('./sounds/main_theme.mp3');
 
-document.getElementById('play-game-button').addEventListener('click', () => {
-    beat.play();
-})
-
 
 // transition between welcome page -> game
 function transition_to_game() {
